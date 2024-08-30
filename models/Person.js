f7ab699676ae8b094cb80ae668e3f8d3c9eb06b1
Debarka <debarka.2k2@gmail.com>
2024-08-31 01:51:10 +0530

    added authentication

--- models/Person.js
+++ models/Person.js
@@ -1,4 +1,5 @@
 const mongoose=require('mongoose')
+const bcrypt=require('bcrypt')
 
 const personSchema=new mongoose.Schema({
     name:{
@@ -30,9 +31,49 @@ const personSchema=new mongoose.Schema({
     salary:{
         type:Number,
         required:true
+    },
+    username:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
     }
 })
 
+//In the below line pre is a middleware function which is run just before save operation
+//next callback will tell moongoose to save in database
+personSchema.pre('save',async function(next){
+    const person=this
+    
+    //We will have to hash the password only when it is new or changed
+    //Whenever password is not changed it will do nothing
+    if(!person.isModified('password')) return next()
+    try{
+        //Hashing password generation
+        const salt=await bcrypt.genSalt(10)
+
+        const hashedPassword=await bcrypt.hash(person.password,salt)
+
+        //Override the plain password with the hashed password
+        person.password=hashedPassword
+        next()
+    }
+    catch(err){
+        return next(err)
+    }
+})
+
+personSchema.methods.comparePassword=async function(candidatePassword){
+    try{
+        const isMatch=await bcrypt.compare(candidatePassword,this.password)
+        return isMatch
+    }
+    catch(error){
+        throw error
+    }
+}
 
 //Creating Person model
 const Person=mongoose.model('Person',personSchema)
--- server.js
+++ server.js
@@ -4,11 +4,30 @@ const db=require('./db')
 const Person=require('./models/Person')
 const MenuItem=require('./models/Menu')
 require('dotenv').config()
+const passport=require('./auth')
+
 
 const bodyParser=require('body-parser')
 app.use(bodyParser.json())//req.body
 
-app.get('/',function(req,res){
+//Middlewares
+//Stores the log when an endpoint is hit
+const logRequest=(req,res,next)=>{
+    console.log(`[${new Date().toLocaleString()}] Request made to: ${req.originalUrl}`)
+    next()   //In every middleware next is important
+}
+
+//Store the log when any endpoint is hit
+app.use(logRequest)
+
+
+//Passport is initialized
+app.use(passport.initialize())
+
+//If we want to store the log when the following endpoint is hit 
+//app.get('/',logRequest,function(req,res){
+const localAuthMiddleware=passport.authenticate('local',{session:false})
+app.get('/',localAuthMiddleware,function(req,res){
     res.send("Hello welcome to the hotel")
 })
 
@@ -25,6 +44,6 @@ app.use('',menuRoutes)
 
 const PORT=process.env.PORT || 3000
 
-app.listen(3000,()=>(
+app.listen(PORT,()=>(
     console.log("Running on 3000")//prints on console
 ))
